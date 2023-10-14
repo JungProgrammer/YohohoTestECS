40500bbd2b90222f35d095baab50037d90ba782b
Artem <atyom.najdanov@mail.ru>
2023-10-14 11:47:55 +0500

    Implemented EcsStartup, added SceneData

---
+++ Assets/_src/CodeBase/EcsCore/EcsStartup.cs
@@ -0,0 +1,47 @@
+﻿using _src.CodeBase.UnityComponents.Data;
+using Leopotam.Ecs;
+using UnityEngine;
+
+namespace _src.CodeBase.EcsCore
+{
+    public class EcsStartup : MonoBehaviour
+    {
+        [SerializeField] 
+        private SceneData _sceneData;
+        
+        
+        private EcsWorld _world;
+        private EcsSystems _systems;
+
+        private void Start()
+        {
+            _world = new EcsWorld();
+            _systems = new EcsSystems(_world);
+		
+#if UNITY_EDITOR
+            Leopotam.Ecs.UnityIntegration.EcsWorldObserver.Create(_world);
+            Leopotam.Ecs.UnityIntegration.EcsSystemsObserver.Create(_systems);
+
+#endif
+            _systems
+                .Inject(_sceneData)
+                .Init();
+        }
+
+        private void Update()
+        {
+            _systems?.Run();
+        }
+
+        private void OnDestroy()
+        {
+            if (_systems != null)
+            {
+                _systems.Destroy();
+                _systems = null;
+                _world.Destroy();
+                _world = null;
+            }
+        }
+    }
+}
---
+++ Assets/_src/CodeBase/UnityComponents/Data/SceneData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace _src.CodeBase.UnityComponents.Data
+{
+    public class SceneData : MonoBehaviour
+    {
+        [SerializeField] 
+        private Transform _heroSpawnPoint;
+
+
+        public Transform HeroSpawnPoint => _heroSpawnPoint;
+    }
+}
