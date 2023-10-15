30776d55025a8091b7b40e70d888fbea467574f5
artemnaydanov <artyom.najdanov@mail.ru>
2023-10-15 13:32:10 +0500

    Implemented PlayerCarrierAnimatorSystem

---
+++ Assets/_src/CodeBase/Ecs/Systems/Animations/PlayerCarrierAnimatorSystem.cs
@@ -0,0 +1,32 @@
+using Leopotam.Ecs;
+using UnityEngine;
+using YohohoTest._src.CodeBase.Ecs.Components.Events;
+using YohohoTest._src.CodeBase.Ecs.Components.Links;
+using YohohoTest._src.CodeBase.Ecs.Components.Objects.Tags;
+using YohohoTest._src.CodeBase.Ecs.Components.StackLogic;
+
+namespace YohohoTest._src.CodeBase.Ecs.Systems.Animations
+{
+    public class PlayerCarrierAnimatorSystem : IEcsRunSystem
+    {
+        private static int Carrying = Animator.StringToHash("Carrying");
+
+        private EcsFilter<PlayerTag, AnimatorLink> _playerFilter;
+        private EcsFilter<HandItemTag, StackPlace> _stackFilter;
+        private EcsFilter<HandItemsCountChangedEvent> _eventFilter;
+
+
+        public void Run()
+        {
+            if (_eventFilter.IsEmpty())
+                return;
+
+
+            foreach (int index in _playerFilter)
+            {
+                ref AnimatorLink animatorLink = ref _playerFilter.Get2(index);
+                animatorLink.Value.SetBool(Carrying, _stackFilter.GetEntitiesCount() > 0);
+            }
+        }
+    }
+}
